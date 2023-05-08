d0b0b55d34fd91b895e991178b5ea10470ceee25
Philipp Schult <0schult@informatik.uni-hamburg.de>
2023-05-08 08:35:49 +0200

    Fix issues with shell programm of week 3

--- MOBS/woche3/myshell.c
+++ MOBS/woche3/myshell.c
@@ -4,16 +4,30 @@
 #include <unistd.h>
 #include <string.h>
 
+void clearInputBuffer() {
+	int c;
+	while((c = getchar()) != '\n') {}
+}
+
 int main(void) {
 	pid_t pid;
 	int cmdnum;
-	char c;
 	char cmd[100], arg[100];
+	int inputcheck;
 	while(1) {
 		printf("Command? \n");
 		printf("Available commands:\n");
 		printf("1: ls\n2: ps\n3: cal\n4: exit\n");
-		scanf("%d", &cmdnum);
+		//Zum Ueberpruefen der Eingabe auf Korrektheit
+		inputcheck = scanf("%d", &cmdnum);
+		
+		//Wenn inputcheck < 1 ist die Eingabe ungueltig/keine Zahl/zu grosse Zahl
+		if(inputcheck < 1) {
+			printf("Invalid input, please try again.\n");
+			//clearInputBuffer(); //Input buffer muss vorher gecleared werden
+			continue; //Wir fangen von Vorne an
+		}
+		//Ablesen der Zahl und korrektes Kommando setzen
 		switch(cmdnum) {
 			case 1:
 				strcpy(cmd, "ls");
@@ -28,26 +42,43 @@ int main(void) {
 				exit(0);
 				break;
 			default:
-				printf("Invalid input\n");
-				continue;
+				printf("Input number does not correspond to a listed command. Please try again.\n");
+				continue; //Eingabe falsch, wir fangen von Vorne an
 		}
 
 		printf("Argument? type 'no' for no argument\n");
-		scanf("%99s", arg);
+		//Zum Ueberpruefen, ob Eingabe zu lang ist
+		int position;
+		scanf("%99s%n", arg, &position);
+		//Wenn die Eingabe zu lang ist, brechen wir ab und clearen den Input buffer
+		if(position >= 99) {
+			printf("Argument too long, command cancelled. Please try again.\n");
+			//clearInputBuffer();
+			continue; //Wir fangen von Vorne an
+		}
+
 		pid = fork();
 		if(pid > 0) {
 			int status;
 			if(wait(&status) == pid && WIFEXITED(status))
-				printf ("Exit Status: %d\n", WEXITSTATUS(status));
+			printf ("Exit Status: %d\n", WEXITSTATUS(status));
+			clearInputBuffer();
 		}
 		else if (pid == 0) {
+			//Wenn 'no' eingegeben wurde, ohne Kommando ausfuehren
 			if(strcmp(arg, "no") == 0) {
 				execlp(cmd, cmd, NULL, NULL);
-				printf("exec failed");
+				printf("exec failed\n");
+				clearInputBuffer();
+				break; //Notwendig weil Prozess bei Fehlschlagen sonst in while-loop bleibt und man 2 exit commands brauchen wuerde
 			}
+			//Sonst mit Kommando
 			else {
 				execlp(cmd, cmd, arg, NULL);
-				printf("exec failed");
+				printf("exec failed\n");
+				clearInputBuffer();
+				break; //Notwendig weil Prozess bei Fehlschlagen sonst in while-loop bleibt und man 2 exit commands brauchen wuerde
+
 			}
 		}
 	}
