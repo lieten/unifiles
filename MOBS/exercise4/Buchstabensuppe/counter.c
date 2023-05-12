4aa3e935179abfac3bd6d152d9e3895e4ec71d94
Philipp Schult <0schult@informatik.uni-hamburg.de>
2023-05-12 16:32:19 +0200

    Added reading file functionality to exercise 4 of MOBS

--- MOBS/exercise4/Buchstabensuppe/counter.c
+++ MOBS/exercise4/Buchstabensuppe/counter.c
@@ -1,9 +1,39 @@
 #include "counting.h" //nicht aendern!
 
+#include <stdio.h>
+#include <stdlib.h>
+
 int alphabet[26]; //nicht aendern!
 
 void count(const char* file_name){ // Name der Inputdatei
 	//Hier koennte Ihre Loesung stehen.
+	FILE *textfile;
+	char *text;
+	long numbytes;
+	
+	textfile = fopen(file_name, "r");
+	if(textfile == NULL) {
+		printf("upps1");
+		return;
+	}
+	fseek(textfile, 0L, SEEK_END);
+	numbytes = ftell(textfile);
+	fseek(textfile, 0L, SEEK_SET);
+	
+	text = (char*)calloc(numbytes, sizeof(char));
+	if(text == NULL) {
+		printf("upps2");
+		return; 
+	}
+
+	fread(text, sizeof(char), numbytes, textfile);
+	fclose(textfile);
+	
+	printf(text);
+	//Bis hierhin wird die Datei eingelesen
 }
 
-
+int main () {
+	count("input.txt");
+	return 0;
+}
