1616127251a8f771b3d4c9c15c2d57d08b1da49b
Philipp Schult <0schult@informatik.uni-hamburg.de>
2023-05-13 12:56:09 +0200

    Continued work on MOBS exercise 4
    TODO: strlen(text) causes segmentation fault

--- MOBS/exercise4/Buchstabensuppe/counter.c
+++ MOBS/exercise4/Buchstabensuppe/counter.c
@@ -2,13 +2,23 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
+#include <string.h>
+
+#define NUMCORES 4
 
 int alphabet[26]; //nicht aendern!
-
-void count(const char* file_name){ // Name der Inputdatei
+char *text;
+/*
+typedef struct params {
+	char **text;
+	int offset;
+} params;
+*/
+void readFile(const char* file_name/*, char **text*/) {
 	//Hier koennte Ihre Loesung stehen.
 	FILE *textfile;
-	char *text;
+	//char *text;
 	long numbytes;
 	
 	textfile = fopen(file_name, "r");
@@ -29,11 +39,51 @@ void count(const char* file_name){ // Name der Inputdatei
 	fread(text, sizeof(char), numbytes, textfile);
 	fclose(textfile);
 	
-	printf(text);
+	//printf(text);
 	//Bis hierhin wird die Datei eingelesen
 }
 
-int main () {
+void *function(void *a) {
+	/*
+	params *input = (params*) a;
+	char **textp = input->text;
+	char *text = *textp;
+	int offset = input->offset;
+	*/
+	int offset = *((int*) a);
+	long textLength;// = strlen(text);
+	printf("%d\n", offset);
+	for(int i = offset; i < textLength; i += NUMCORES) {
+		printf("%c", text[i]);
+	}
+	printf("\n");
+	free(a);
+}
+
+void startThread(int offset/*, char **text*/) {
+	pthread_t tid;
+	/*
+	params *args = malloc(sizeof(params));
+	args->offset = offset;
+	args->text = text;
+	*/
+	int *param = malloc(sizeof(int));
+	*param = offset;
+	int success = pthread_create(&tid, NULL, &function, (void*) param);
+	printf("Success: %d\n", success);
+}
+
+void count(const char* file_name) {
+	//char *text;
+	//readFile(file_name, &text);
+	printf("%d\n", strlen(text));
+	readFile(file_name);
+	//printf(text);
+	//startThread(0, &text);
+	startThread(0);
+}
+
+int main() {
 	count("input.txt");
 	return 0;
 }
